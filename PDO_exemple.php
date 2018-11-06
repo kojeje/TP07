8f4a6965e90c7d3edcdc1a20e751d5665478df3c
kojeje <kojeje.dev@gmail.com>
2018-11-06 11:20:57 +0100

    ex pdo

--- PDO_exemple.php
+++ PDO_exemple.php
@@ -5,29 +5,43 @@ header('Content-type: text/plain; charset=utf-8;');
 
 $dbName = 'bibliotheque';
 $dbUser = 'root';
-$dbPassword = 'masteruser';
+$dbPassword = 'root';
 
 $dsn = "mysql:host=localhost;dbname=$dbName";
 
 try {
     $dbh = new PDO($dsn, $dbUser, $dbPassword);
-    
+
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
+
     $sql = 'SELECT * ';
-    $sql .= 'FROM auteur, ';
-    $sql .= 'WHERE nom = :nom';
-    
+    $sql .= 'FROM livre ';
+    $sql .= 'WHERE annee > :annee';
+
     $params = array(
-        ':nom' => 'kundera'
+        ':annee' => '1990'
     );
-    
+
     try {
         $stt = $dbh->prepare($sql);
+
         $stt->execute($params);
+
+        $resultat = $stt->fetchAll();
+
+        foreach ($resultat AS $livre) {
+            printf("id %s \n titre %s \n resume %s \n annee %s \n", $livre['id'], $livre['titre'], $livre['resume'], $livre['annee']);
+
+        }
+
+
     } catch (PDOException $e) {
         echo 'Query error : ' . $e->getMessage();
+
     }
+
 } catch (PDOException $e) {
-    echo 'Connection error : ' . $e->getMessage();
+    echo 'Query error : ' . $e->getMessage();
+
 }
+
