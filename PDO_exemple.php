aab27e0ab21ac6175679fa6b208e93bbbbc2bc95
kojeje <kojeje.dev@gmail.com>
2018-11-06 15:18:54 +0100

    ex pdo

--- PDO_exemple.php
+++ PDO_exemple.php
@@ -12,17 +12,29 @@ $dsn = "mysql:host=localhost;dbname=$dbName";
 try {
     $dbh = new PDO($dsn, $dbUser, $dbPassword);
 
+//Pour réagir en cas d'erreur (à copier coller tel quel)
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+
+//attribuer requete à vaiable $sql
     $sql = 'SELECT * ';
+
+//NB '.=' concaténé aux valeurs precedentes
     $sql .= 'FROM livre ';
-    $sql .= 'WHERE annee > :annee';
+//    $sql .= 'WHERE annee > :year';
+//    ou
+    $sql .= 'WHERE annee > :year_down AND annee < :year_up';
 
     $params = array(
-        ':annee' => '1990'
+
+
+        ':year_down' => '1990',
+        ':year_up' => '2005'
     );
 
     try {
+
+//        Envoyer la requete à la base de données
         $stt = $dbh->prepare($sql);
 
         $stt->execute($params);
@@ -30,11 +42,13 @@ try {
         $resultat = $stt->fetchAll();
 
         foreach ($resultat AS $livre) {
-            printf("id %s \n titre %s \n resume %s \n annee %s \n", $livre['id'], $livre['titre'], $livre['resume'], $livre['annee']);
+            
+            printf ("id %s \n titre %s \n resume %s \n  annee %s \n", $livre['id'], $livre['titre'], $livre['resume'], $livre['annee']);
 
         }
 
-
+//        En cas d'erreur'
+//            renvoi d'exeption
     } catch (PDOException $e) {
         echo 'Query error : ' . $e->getMessage();
 
